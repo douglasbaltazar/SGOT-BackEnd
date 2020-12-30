95b9f880ccbe6115d7b35f0e131dcdf1ae901967
Douglas Baltazar <douglasbaltazar1@gmail.com>
2020-12-30 16:23:57 -0300

    #1 Criação do projeto

---
+++ src/main/java/br/com/emiteai/SgotBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.emiteai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgotBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgotBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emiteai/SgotBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emiteai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgotBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
