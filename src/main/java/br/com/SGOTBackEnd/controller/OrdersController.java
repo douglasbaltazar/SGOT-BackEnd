a83868ded91ae0e365cf15680f830e76b88ff4ea
Douglas Baltazar <douglasbaltazar1@gmail.com>
2020-12-30 23:21:29 -0300

    #4 Fix errors

--- src/main/java/br/com/SGOTBackEnd/controller/OrdersController.java
+++ src/main/java/br/com/SGOTBackEnd/controller/OrdersController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import br.com.SGOTBackEnd.exceptions.ResourceNotFoundException;
 import br.com.SGOTBackEnd.model.Orders;
 import br.com.SGOTBackEnd.repository.OrdersRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class OrdersController {
--- src/main/java/br/com/SGOTBackEnd/controller/TransportOrderController.java
+++ src/main/java/br/com/SGOTBackEnd/controller/TransportOrderController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import br.com.SGOTBackEnd.exceptions.ResourceNotFoundException;
 import br.com.SGOTBackEnd.model.TransportOrder;
 import br.com.SGOTBackEnd.repository.TransportOrderRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class TransportOrderController {
